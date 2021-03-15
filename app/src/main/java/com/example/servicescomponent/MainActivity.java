3732806c71cc8496f29a0f7a581c95104e7137d4
Bilal Yousaf <F2017266297@umt.edu.pk>
2021-03-15 17:54:31 +0500

    some changes

--- app/src/main/java/com/example/servicescomponent/MainActivity.java
+++ app/src/main/java/com/example/servicescomponent/MainActivity.java
@@ -3,17 +3,35 @@ package com.example.servicescomponent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.security.Provider;
 import java.security.Provider.Service;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button startService,stopService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ServiceClass serviceClass = new ServiceClass();
+        startService = findViewById(R.id.startServiceBtn);
+        stopService = findViewById(R.id.stopServiceBtn);
+
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceClass.onCreate();
+            }
+        });
+
+        stopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceClass.onDestroy();
+            }
+        });
     }
 }
